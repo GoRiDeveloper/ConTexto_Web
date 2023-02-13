a82446f7552592a1ac58bd821555ba630df2cf3d
Bernardo Sciarretta <basciarretta@hotmail.com>
2023-02-13 14:24:02 -0300

    Cards + video + tips

---
+++ README.md
@@ -0,0 +1,2 @@
+# ConTexto_Web
+Una página dedicada a la información de venta de toallitas y productos de protección intíma femenina biodegradables
---
+++ assets/js/variables.js
@@ -0,0 +1,5 @@
+const 
+
+D             = document,
+NAVBAR        = D.querySelector(".navbar"),
+NAVBAR_BUTTON = D.querySelector(".navbar-toggler");
